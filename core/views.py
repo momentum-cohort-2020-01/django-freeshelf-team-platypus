1345acedc9563fac7172958cb18059155f3bba1c
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-03-01 17:01:06 -0500

    Sorting options added to list and category pages

--- core/views.py
+++ core/views.py
@@ -4,34 +4,25 @@ from .models import Book, Category
 
 
 def book_list(request):
+    sort_option = request.GET.get('sort', 'date')
     books = Book.objects.all()
-    context = {'books': books, 'request': request}
-    return render(request, 'core/book_list.html', context=context)
-
-
-def sort_list(request, option):
-    books = Book.objects.all()
-    books = sort_by(books, option)
+    books = sort_by(books, sort_option)
     context = {'books': books, 'request': request}
     return render(request, 'core/book_list.html', context=context)
 
 
 def cat_list(request, cat):
+    sort_option = request.GET.get('sort', 'date')
     category = Category.objects.get(slug=cat)
     books = Book.objects.filter(category=category)
-    context = {'books': books, 'request': request}
-    return render(request, 'core/book_list.html', context=context)
-
-
-def sort_cat_list(request, cat, option):
-    category = Category.objects.get(slug=cat)
-    books = Book.objects.filter(category=category)
-    if option == 'created_at_reverse':
-        option = '-created_at'
-    books = sort_by(books, option)
+    books = sort_by(books, sort_option)
     context = {'books': books, 'request': request}
     return render(request, 'core/book_list.html', context=context)
 
 
 def sort_by(queryset, option):
-    return queryset.order_by(option)
+    options = {'date': 'created_at',
+               'date-reverse': '-created_at',
+               'author': 'author',
+               'title': 'title'}
+    return queryset.order_by(options[option])
