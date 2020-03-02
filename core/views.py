7e2a2f19a2aa40a7d736c3447ef1bdce8720be24
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-03-02 14:55:12 -0500

    Adds suggestion list view for logged in peeps

--- core/views.py
+++ core/views.py
@@ -5,23 +5,34 @@ from django.http import HttpResponse
 from .models import Book, Category
 
 
-
-
 def book_list(request):
     sort_option = request.GET.get('sort', 'date')
-    books = Book.objects.all()
+    books = Book.objects.filter(approved=True)
     books = sort_by(books, sort_option)
-    context = {'books': books, 'request': request, 'categories': Category.objects.all()}
+    context = {'books': books, 'request': request,
+               'categories': Category.objects.all()}
     return render(request, 'core/book_list.html', context=context)
 
 
 def cat_list(request, cat):
     sort_option = request.GET.get('sort', 'date')
     category = Category.objects.get(slug=cat)
-    books = Book.objects.filter(category=category)
+    books = Book.objects.filter(category=category, approved=True)
     books = sort_by(books, sort_option)
-    context = {'books': books, 'request': request, 'categories': Category.objects.all()}
+    context = {'books': books, 'request': request,
+               'categories': Category.objects.all()}
     return render(request, 'core/book_list.html', context=context)
+
+
+@login_required(login_url='/accounts/login/')
+def suggestion_list(request):
+    sort_option = request.GET.get('sort', 'date')
+    books = Book.objects.filter(approved=False)
+    books = sort_by(books, sort_option)
+    context = {'books': books, 'request': request,
+               'categories': Category.objects.all()}
+    return render(request, 'core/book_list.html', context=context)
+
 
 @login_required(login_url='/accounts/login/')
 def book_suggestion(request):
@@ -29,11 +40,11 @@ def book_suggestion(request):
         form = SuggestionBook(request.POST)
         if form.is_valid():
             book = form.save()
-            # book.save()
         return redirect('book_list')
     else:
         form = SuggestionBook()
     return render(request, 'core/book_suggestion.html', {'form': form})
+
 
 def sort_by(queryset, option):
     options = {'date': 'created_at',
@@ -41,6 +52,7 @@ def sort_by(queryset, option):
                'author': 'author',
                'title': 'title'}
     return queryset.order_by(options[option])
+
 
 def log_in(request):
     return render(request, 'core/log_in.html')
